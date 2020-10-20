7db889451b6b3fcc1e98f3918de392792ef78734
DefinitelyNotAGoat <baldrich@protonmail.com>
2020-10-19 20:56:32 -0400

    cleaner block state

--- internal/watcher/watcher.go
+++ internal/watcher/watcher.go
@@ -10,68 +10,97 @@ import (
 	"github.com/sirupsen/logrus"
 )
 
+type cycleChan struct {
+	Hash  string
+	Cycle int
+}
+
 type Watcher struct {
-	r        rpc.IFace
-	notifier twilio.IFace
-	delegate string
+	r                           rpc.IFace
+	notifier                    twilio.IFace
+	delegate                    string
+	endorsementCurrentBlockChan chan *rpc.Block
+	bakingCurrentBlockChan      chan *rpc.Block
+	currentCurrentBlockChan     chan *rpc.Block
 }
 
 func New(delegate string, r rpc.IFace, notifier twilio.IFace) *Watcher {
 	return &Watcher{
-		r:        r,
-		notifier: notifier,
-		delegate: delegate,
+		r:                           r,
+		notifier:                    notifier,
+		delegate:                    delegate,
+		endorsementCurrentBlockChan: make(chan *rpc.Block, 5),
+		bakingCurrentBlockChan:      make(chan *rpc.Block, 5),
+		currentCurrentBlockChan:     make(chan *rpc.Block, 5),
 	}
+}
+
+func (w *Watcher) startStateUpdater() {
+	ticker := time.NewTicker(time.Second * 30)
+	currentBlock := 0
+	currentCycle := 0
+	go func() {
+		for range ticker.C {
+			head, err := w.r.Head()
+			if err != nil {
+				logrus.WithField("error", err.Error()).Error("Failed to get current block height.")
+			}
+
+			if head.Metadata.Level.Cycle > currentCycle {
+				logrus.WithFields(logrus.Fields{"cycle": head.Metadata.Level.Cycle, "level": head.Header.Level, "hash": head.Hash}).Info("Updated to new cycle.")
+				currentCycle = head.Metadata.Level.Cycle
+				w.currentCurrentBlockChan <- head
+			}
+
+			if head.Header.Level > currentBlock {
+				logrus.WithFields(logrus.Fields{"level": head.Header.Level, "hash": head.Hash}).Info("Updated head block.")
+				currentBlock = head.Header.Level
+
+				w.endorsementCurrentBlockChan <- head
+				w.bakingCurrentBlockChan <- head
+			}
+		}
+	}()
 }
 
 func (w *Watcher) Start() {
 	logrus.Info("Starting baking monitor.")
+	w.startStateUpdater()
 	go func() {
-		t := time.NewTicker(time.Minute)
-		currentCycle := 0
 		var doneBr, doneEr chan struct{}
-		for range t.C {
-			head, err := w.r.Head()
+		for block := range w.currentCurrentBlockChan {
+			logrus.WithField("cycle", block.Metadata.Level.Cycle).Info("Updating current cycle rights.")
+			if doneBr != nil && doneEr != nil {
+				doneEr <- struct{}{}
+				doneBr <- struct{}{}
+			}
+
+			br, err := w.r.BakingRights(rpc.BakingRightsInput{
+				BlockHash:   block.Hash,
+				Cycle:       block.Metadata.Level.Cycle,
+				Delegate:    w.delegate,
+				MaxPriority: 0,
+			})
 			if err != nil {
-				logrus.WithField("error", err.Error()).Error("Failed to get current block height.")
+				logrus.WithFields(logrus.Fields{"error": err.Error(), "cycle": block.Metadata.Level.Cycle}).Error("failed to get baking rights")
 				break
 			}
-			if head.Metadata.Level.Cycle > currentCycle {
-				logrus.WithField("cycle", head.Metadata.Level.Cycle).Info("Updating current cycle rights.")
-				if doneBr != nil && doneEr != nil {
-					doneEr <- struct{}{}
-					doneBr <- struct{}{}
-				}
+			logrus.WithFields(logrus.Fields{"rights": fmt.Sprintf("%+v", br), "cycle": block.Metadata.Level.Cycle}).Info("Received baking rights for cycle.")
+			doneBr = make(chan struct{})
+			w.watchBakingRights(br, doneBr)
 
-				br, err := w.r.BakingRights(rpc.BakingRightsInput{
-					BlockHash:   head.Hash,
-					Cycle:       head.Metadata.Level.Cycle,
-					Delegate:    w.delegate,
-					MaxPriority: 0,
-				})
-				if err != nil {
-					logrus.WithFields(logrus.Fields{"error": err.Error(), "cycle": head.Metadata.Level.Cycle}).Error("failed to get baking rights")
-					break
-				}
-				logrus.WithFields(logrus.Fields{"rights": fmt.Sprintf("%+v", br), "cycle": head.Metadata.Level.Cycle}).Info("Received baking rights for cycle.")
-				doneBr = make(chan struct{})
-				w.watchBakingRights(br, doneBr)
-
-				er, err := w.r.EndorsingRights(rpc.EndorsingRightsInput{
-					BlockHash: head.Hash,
-					Cycle:     head.Metadata.Level.Cycle,
-					Delegate:  w.delegate,
-				})
-				if err != nil {
-					logrus.WithFields(logrus.Fields{"error": err.Error(), "cycle": head.Metadata.Level.Cycle}).Error("failed to get endorsing rights")
-					break
-				}
-				logrus.WithFields(logrus.Fields{"rights": fmt.Sprintf("%+v", er), "cycle": head.Metadata.Level.Cycle}).Info("Received endorsement rights for cycle.")
-				doneEr = make(chan struct{})
-				w.watchEndorsements(er, doneEr)
-
-				currentCycle = head.Metadata.Level.Cycle
+			er, err := w.r.EndorsingRights(rpc.EndorsingRightsInput{
+				BlockHash: block.Hash,
+				Cycle:     block.Metadata.Level.Cycle,
+				Delegate:  w.delegate,
+			})
+			if err != nil {
+				logrus.WithFields(logrus.Fields{"error": err.Error(), "cycle": block.Metadata.Level.Cycle}).Error("failed to get endorsing rights")
+				break
 			}
+			logrus.WithFields(logrus.Fields{"rights": fmt.Sprintf("%+v", er), "cycle": block.Metadata.Level.Cycle}).Info("Received endorsement rights for cycle.")
+			doneEr = make(chan struct{})
+			w.watchEndorsements(er, doneEr)
 		}
 	}()
 }
@@ -79,25 +108,18 @@ func (w *Watcher) Start() {
 func (w *Watcher) watchBakingRights(br *rpc.BakingRights, done chan struct{}) {
 	uuid := uuid.New()
 	logrus.WithField("uuid", uuid.String()).Info("Starting new baking rights worker.")
-	t := time.NewTicker(time.Minute)
 	go func() {
 		for {
 			select {
-			case <-t.C:
-				head, err := w.r.Head()
-				if err != nil {
-					logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "error": err.Error()}).Error("Failed to get current block height.")
-				}
-				logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "block": head.Header.Level}).Info("Baking rights worker found new block.")
-
+			case block := <-w.bakingCurrentBlockChan:
 				for _, r := range *br {
-					if r.Level == head.Header.Level && r.Priority == 0 {
-						logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "block": head.Header.Level}).Info("Baking rights worker found baking slot for priority 0.")
-						if head.Metadata.Baker != w.delegate {
-							logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "block": head.Header.Level}).Error("Missed Baking Opportunity.")
-							w.notifier.Send(fmt.Sprintf("Missed Baking Opportunity at level '%d'.", head.Header.Level))
+					if r.Level == block.Header.Level && r.Priority == 0 {
+						logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "block": block.Header.Level}).Info("Baking rights worker found baking slot for priority 0.")
+						if block.Metadata.Baker != w.delegate {
+							logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "block": block.Header.Level}).Error("Missed Baking Opportunity.")
+							w.notifier.Send(fmt.Sprintf("Missed Baking Opportunity at level '%d'.", block.Header.Level))
 						} else {
-							logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "block": head.Header.Level}).Info("Successfully Baked Block.")
+							logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "block": block.Header.Level}).Info("Successfully Baked Block.")
 						}
 					}
 				}
@@ -112,23 +134,16 @@ func (w *Watcher) watchBakingRights(br *rpc.BakingRights, done chan struct{}) {
 func (w *Watcher) watchEndorsements(er *rpc.EndorsingRights, done chan struct{}) {
 	uuid := uuid.New()
 	logrus.WithField("uuid", uuid.String()).Info("Starting new endorsement rights worker.")
-	t := time.NewTicker(time.Minute)
 
 	go func() {
 		for {
 			select {
-			case <-t.C:
-				head, err := w.r.Head()
-				if err != nil {
-					logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "error": err.Error()}).Error("Failed to get current block height.")
-				}
-				logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "block": head.Header.Level}).Info("Endorsing rights worker found new block.")
-
+			case block := <-w.endorsementCurrentBlockChan:
 				for _, r := range *er {
-					if r.Level == head.Header.Level-1 {
-						logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "right-level": r.Level, "block": head.Header.Level}).Info("Endorsing rights worker found endorsing slot.")
+					if r.Level == block.Header.Level-1 {
+						logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "right-level": r.Level, "block": block.Header.Level}).Info("Endorsing rights worker found endorsing slot.")
 						var found bool
-						for _, operations := range head.Operations {
+						for _, operations := range block.Operations {
 							for _, operation := range operations {
 								contents := operation.Contents.Organize()
 								for _, endorsement := range contents.Endorsements {
@@ -139,14 +154,15 @@ func (w *Watcher) watchEndorsements(er *rpc.EndorsingRights, done chan struct{})
 							}
 						}
 						if !found {
-							logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "right-level": r.Level, "block": head.Header.Level}).Error("Missed Endorsing Opportunity.")
+							logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "right-level": r.Level, "block": block.Header.Level}).Error("Missed Endorsing Opportunity.")
 							w.notifier.Send(fmt.Sprintf("Missed Endorsement Opportunity at level '%d'", r.Level))
 						} else {
-							logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "right-level": r.Level, "block": head.Header.Level}).Info("Successfully Endorsed Block.")
+							logrus.WithFields(logrus.Fields{"uuid": uuid.String(), "right-level": r.Level, "block": block.Header.Level}).Info("Successfully Endorsed Block.")
 						}
 					}
 				}
 			case <-done:
+				logrus.WithField("uuid", uuid.String()).Info("Ending endorsing rights worker.")
 				break
 			}
 		}
